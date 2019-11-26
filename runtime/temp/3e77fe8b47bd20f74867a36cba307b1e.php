66ddea12d4c511e7da2c8090e7ca75df746970d5
zhanghan <zhanghan@qq.com>
2019-11-26 11:51:36 +0800

    '首页'

--- runtime/temp/3e77fe8b47bd20f74867a36cba307b1e.php
+++ runtime/temp/3e77fe8b47bd20f74867a36cba307b1e.php
@@ -1,4 +1,4 @@
-<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:67:"C:\phpEnv\www\hui-\public/../application/home\view\index\index.html";i:1574678760;}*/ ?>
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:67:"C:\phpEnv\www\hui-\public/../application/home\view\index\index.html";i:1574737402;}*/ ?>
 <!DOCTYPE>
 <html lang="en">
 
@@ -383,7 +383,7 @@
 				<div class='success_title'></div>
 				<div class='success_icon'>
 					<?php if(is_array($case_list) || $case_list instanceof \think\Collection || $case_list instanceof \think\Paginator): $i = 0; $__LIST__ = $case_list;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$data_list): $mod = ($i % 2 );++$i;?>
-					<div onclick="click_show(this)" keys="<?php echo $key; ?>" class="<?php echo $data_list['is_show'].$key; ?>" data="<?php echo $count; ?>"
+					<div style="cursor:pointer;" onclick="click_show(this)" keys="<?php echo $key; ?>" class="<?php echo $data_list['is_show'].$key; ?>" data="<?php echo $count; ?>"
 						data-attr="<?php echo $data_list['is_show']; ?>">
 						<div class='<?php if($key == 1): ?>hui_icon<?php else: ?>p_icon<?php endif; ?>'>
 							<div><?php echo $data_list['title2']; ?></div>
